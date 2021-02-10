a1e371faf88f8d3dc8e6e827ba7ca0140b888d91
glenndavidson0 <75863359+glenndavidson0@users.noreply.github.com>
2021-02-09 21:07:36 -0800

    Update generateScenario.sh

--- Scripts/Input_Generator/generateScenario.sh
+++ Scripts/Input_Generator/generateScenario.sh
@@ -1,3 +1,7 @@
+# make folder "output" if it doesn't exist already
+mkdir -p output
+
+# print information for user
 echo
 echo "Compiling json post processor:"
 make
@@ -5,7 +9,9 @@ echo
 echo "Generating neighborhoods:"
 echo "NOTE - 'Invalid dauid found' indicates a cell with zero population, not an error"
 echo
+# create pre_preprocessing.json
 python generate_cadmium_json.py
+# create format pre_preprocessing.json
 echo
 echo "Running json post processor:"
 ./convertScenario input/pre_processing.json
