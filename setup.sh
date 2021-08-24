42d0ca2ed8ecfc0e8a34aa5d5e1f174b40e466da
Glenn Davidson <glenntdavidson@gmail.com>
2021-08-24 00:34:17 -0700

    plotting updates

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 # Run while setting up the model, only needs to be run once
-#cmake "CMakeLists.txt"
-#make
-cd Scripts/Transform_Case_Data
+cmake "CMakeLists.txt"
+make
+cd Scripts/Transform_Case_Data/pickled_cases
+tar -xf sorted_data_per_region.tar.xz
+tar -xf region_labels.tar.xz
+cd ..
 python transform_case_data.py
 python plot_all_phu_individually.py
 python plot_all_phu_infected.py
